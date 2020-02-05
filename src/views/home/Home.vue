969098d1eeef44b531edd2ab9327ceec49a2d541
ZeyuGuo <954117348@qq.com>
2020-02-05 21:21:14 +0800

    add navbar

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,16 +1,17 @@
 <template>
   <div id="home">
+    <nav-bar class="home-nav"><div slot="center">首页</div></nav-bar>
     <h2>Home Page</h2>
   </div>
 </template>
 
 <script>
-  
+  import NavBar from 'components/common/navbar/NavBar'
 
   export default {
     name: "Home",
     components: {
-      
+      NavBar
     },
     data() {
       return {
@@ -28,5 +29,9 @@
 </script>
 
 <style scoped>
+  .home-nav {
+    background-color: var(--color-tint);
+    color:#ffffff;
+  }
 
 </style>
